ab227a8994d7174f55abbf90548246aa7b023fde
Chiara Batelić <cbatelic@unipu.hr>
2021-05-04 00:16:42 +0200

    Specifikacija backenda

--- src/index.js
+++ src/index.js
@@ -1,11 +1,66 @@
-import express from 'express'
+import express from 'express';
+import cors from 'cors';
+import data from './store';
 
 const app = express()
 const port = 3000
 
-app.get('/', (req,res) => {
-    res.send("Hello u browseru")
-    console.log("Hello u konzoli");
-})
+app.use(cors())
+app.use(express.json());
 
-app.listen(port, () => console.log(`Port ${port}`))
+app.get('/', (req, res) => {
+    res.json('Home page!');
+  });
+
+app.post('/users', (req, res) => res.json(data.users));
+
+app.get('/users', (req, res) => res.json(data.users));
+
+app.post('/terrain', (req, res) => {
+    res.json({});
+  });
+
+app.get('/terrain', (req, res) => {
+    res.json(data.terrain);
+  });
+
+app.get('/terrain/categories', (req, res) => {
+    res.json(data.categories);
+  });
+
+app.get('/terrain/categories/termin', (req, res) => {
+    res.json(data.termin);
+  });
+
+app.post('/terrain/categories/termin/teamReg', (req, res) => {
+    res.json({});
+  });
+
+app.get('/terrain/categories/termin/teamReg', (req, res) => {
+    res.json(data.team);
+  });
+
+app.post('/teams', (req, res) => {
+    res.json({});
+  });
+
+app.get('/teams', (req, res) => {
+    res.json({});
+  });
+
+app.post('/messages', (req, res) => {
+    res.json({});
+  });
+
+app.get('/messages', (req, res) => {
+    res.json({});
+  });
+
+app.get('/search', (req, res) => {
+    res.json({});
+  });
+
+
+app.listen(port, () =>
+  console.log(`\n\n[DONE] Backend se vrti na http://localhost:${port}/\n\n`)
+);
---
+++ src/store.js
@@ -0,0 +1,48 @@
+let data = {
+    
+    users: {
+        data: [
+            {
+                name: "",
+                surname: "",
+                username: "",
+                email: "",
+                password: "",
+            },
+        ],
+    },
+    terrain: {
+        data: [
+            {
+                name_hall:"",
+                name_city:"",
+            }
+        ]
+    },
+    categories: {
+        data: [
+            {
+                categories:""
+            }
+        ]
+    },
+    termin: {
+        data: [
+            {
+                date:"",
+                time:""
+            }
+        ]
+    },
+    team: {
+        data: [
+            {
+                team_name:"",
+                members:"",
+                note:""
+            }
+        ]
+    },
+
+}
+export default data;
